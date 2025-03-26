957f5f6a04ce7ea6d7e7270efda17c44c814be9c
Chris Cortez <chris037@users.noreply.github.com>
2025-03-27 02:26:12 +0800

    Allow Render domain in preview config 2.26

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  
+  base: '/evp/',
   plugins: [react()],
   server: {
     host: '0.0.0.0', // ✅ Required for local dev and Render to expose port
