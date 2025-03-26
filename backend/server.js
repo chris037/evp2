721cdec72362896239c0c144d8629441c19cb413
Chris Cortez <chris037@users.noreply.github.com>
2025-03-27 00:31:23 +0800

    Initial backend commit

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const taskRoutes = require("./routes/taskRoutes");
 const dreamwallRoutes = require("./routes/dreamwallRoutes");
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 // ✅ Middleware
 app.use(cors());
