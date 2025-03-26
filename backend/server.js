e0c015dc57b65d406db6d3fee7ed81e7b1e52a13
Chris Cortez <chris037@users.noreply.github.com>
2025-03-27 06:40:31 +0800

    server update 1.9 server.js

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 //✅ Allow all origins or specify your frontend domain
-app.use(cors({
-  origin: ["https://evp-xs9f.onrender.com", "https://ibexvst.com", "http://localhost:4173", "http://10.210.123.172:4173"], // ✅ Allow your frontend
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  headers: ["Content-Type"],
-  credentials: true,
-}));
+app.use(cors());
+// app.use(cors({
+//   origin: ["https://evp-xs9f.onrender.com", "https://ibexvst.com", "http://localhost:4173", "http://10.210.123.172:4173"], // ✅ Allow your frontend
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   headers: ["Content-Type"],
+//   credentials: true,
+// }));
 
 const bodyParser = require("body-parser");
 const path = require("path");
